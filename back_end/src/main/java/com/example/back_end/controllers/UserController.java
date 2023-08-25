de437d15ce1e0b247351f9e4f2ba3453bc73ae8b
Mateus Mendes <matreuusmendes@gmail.com>
2023-08-25 19:17:23 -0300

    rota atualizarUsuario, método PUT

--- back_end/src/main/java/com/example/back_end/controllers/UserController.java
+++ back_end/src/main/java/com/example/back_end/controllers/UserController.java
@@ -5,10 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.back_end.models.User;
 import com.example.back_end.repository.UserRepository;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -34,6 +37,26 @@ public class UserController {
         else{
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+    }
+
+    // Método editar Usuário
+    // Passar nome, email ou senha ( ou todos ) no body de sua requisição
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public User atualizarUsuario(@RequestBody User dadosNovos, @PathVariable("id") Long id){
+        User usuario = userRepository.findById(id).get();
+        if (dadosNovos.getName() != null){
+            usuario.setName(dadosNovos.getName());
+        }
+        if (dadosNovos.getEmail() != null){
+            usuario.setEmail(dadosNovos.getEmail());
+        }
+
+        if (dadosNovos.getPassword() != null){
+            usuario.setPassword(dadosNovos.getPassword());
+        }
+        userRepository.save(usuario);
+
+        return usuario;
     }
     
 }
