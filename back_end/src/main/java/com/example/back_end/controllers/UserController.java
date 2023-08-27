b6535574e8c4d51c80e318a7f21e4ab5c9d89a1a
Mateus Mendes <matreuusmendes@gmail.com>
2023-08-27 11:43:47 -0300

    Tratamento de Erros

--- back_end/src/main/java/com/example/back_end/controllers/UserController.java
+++ back_end/src/main/java/com/example/back_end/controllers/UserController.java
@@ -6,11 +6,14 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.back_end.models.User;
 import com.example.back_end.repository.UserRepository;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,9 +30,17 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getUser(@PathVariable("id") Long id){
+        Optional<User> usuario = userRepository.findById(id);
+        if(usuario.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(usuario.get());
+        }
+        return ResponseEntity.badRequest().body("Usuário não encontrado");
+    }
+
     @PostMapping
     public ResponseEntity<User> cadastrarUsuario(@RequestBody User user){
-        System.out.println(user.getName());
         if (userRepository.findByEmail(user.getEmail()) == null){
             User usuario = userRepository.save(user);
             return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
@@ -42,21 +53,29 @@ public class UserController {
     // Método editar Usuário
     // Passar nome, email ou senha ( ou todos ) no body de sua requisição
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public User atualizarUsuario(@RequestBody User dadosNovos, @PathVariable("id") Long id){
-        User usuario = userRepository.findById(id).get();
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> atualizarUsuario(@RequestBody User dadosNovos, @PathVariable("id") Long id){
+
+        Optional<User> usuario = userRepository.findById(id);
+        if (!usuario.isPresent()){
+            return ResponseEntity.badRequest().body("Usuário não encontrado");
+        }
+
+        User usuarioEncontrado = usuario.get();
+
         if (dadosNovos.getName() != null){
-            usuario.setName(dadosNovos.getName());
+            usuarioEncontrado.setName(dadosNovos.getName());
         }
         if (dadosNovos.getEmail() != null){
-            usuario.setEmail(dadosNovos.getEmail());
+            usuarioEncontrado.setEmail(dadosNovos.getEmail());
         }
-
         if (dadosNovos.getPassword() != null){
-            usuario.setPassword(dadosNovos.getPassword());
+            usuarioEncontrado.setPassword(dadosNovos.getPassword());
         }
-        userRepository.save(usuario);
+        
+        userRepository.save(usuarioEncontrado);
 
-        return usuario;
+        return ResponseEntity.status(HttpStatus.OK).body(usuario);
     }
     
 }
