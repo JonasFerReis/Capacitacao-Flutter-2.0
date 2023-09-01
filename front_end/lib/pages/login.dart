c73f861c1602a8f6021270061fd4a3b192e65342
Lara Linhares <lararlinhares@gmail.com>
2023-08-31 23:15:39 -0300

    ajuste tela

--- front_end/lib/pages/login.dart
+++ front_end/lib/pages/login.dart
@@ -35,7 +35,7 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               children: [
         
-                const SizedBox(height: 12),
+                const SizedBox(height: 100),
                 //ícone de usuário
                 const Icon(
                   Icons.person_outline,
@@ -148,7 +148,7 @@ class _LoginPageState extends State<LoginPage> {
                   ],
                 ),
                 
-                const SizedBox(height: 30),
+                const SizedBox(height: 35),
 
                 //ícone da emakers
                 const Row(
