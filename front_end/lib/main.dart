ba24f16b5410dc158d005eea083ee71a498d0b66
danilochagasc@example.com <danilovga1000@gmail.com>
2023-08-31 21:41:14 -0300

    updates

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/login.dart';
+import 'package:front_end/pages/initial_screen.dart';
 
 void main(){
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: InitialScreen(),
     );
   }
 }
---
+++ front_end/lib/pages/register_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:front_end/components/my_textfield.dart';
+
+import '../components/register_button.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final emailController = TextEditingController();
+    final nameController = TextEditingController();
+    final passwordController = TextEditingController();
+    final confirmPasswordController = TextEditingController();
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 102, 52, 116),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                //icone com espaçamento em cima e em baixo
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 50),
+                  child: Icon(
+                    Icons.person_outline,
+                    size: 100,
+                    color: Color.fromARGB(255, 255, 252, 252),
+                  ),
+                ),
+
+                //Email TextField
+                MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 15),
+
+                //Username TextField
+                MyTextField(
+                  controller: nameController,
+                  hintText: 'Nome',
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 15),
+
+                //Password TextField
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Senha',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 15),
+
+                //Confirm Password TextField
+                MyTextField(
+                  controller: confirmPasswordController,
+                  hintText: 'Senha',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 15),
+
+                //Botão "Entrar"
+                const RegisterButton(),
+
+                Image.asset(
+                  'lib/images/pc_com_chip_1.png',
+                  height: 300,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
