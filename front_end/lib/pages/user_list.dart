a1002a4eeff28068c31e71035e27cdab18fdb8e5
Danilo Chagas <75141085+danilochagasc@users.noreply.github.com>
2023-10-24 16:22:15 -0300

    metodos post e get funcionando

--- front_end/lib/pages/user_list.dart
+++ front_end/lib/pages/user_list.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:front_end/data/user_inherited.dart';
-
 import '../components/user_card.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -13,23 +11,23 @@ class UsersList extends StatefulWidget {
 }
 
 class _UsersListState extends State<UsersList> {
-
-  //lista de usuarios
+  // Lista de usuários
   List<UserCard> usList = [];
 
-  //funcao que pega os dados do banco de dados e adiciona na lista
+  // Função que pega os dados do banco de dados e adiciona na lista
   void getUsers() async {
     final response = await http.get(Uri.parse('http://10.0.2.2:8080/users'));
     final data = jsonDecode(response.body);
     for (var i = 0; i < data.length; i++) {
-      print(data[i]['name']);
-      print(data[i]['email']);
       usList.add(UserCard(
-          userName: data[i]['name'],
-          userEmail: data[i]['email'],
-          userImage:
-              'https://w7.pngwing.com/pngs/851/512/png-transparent-looking-at-the-cat-hand-painted-pet-cat.png'));
+        userName: data[i]['name'],
+        userEmail: data[i]['email'],
+        userImage:
+        'https://w7.pngwing.com/pngs/851/512/png-transparent-looking-at-the-cat-hand-painted-pet-cat.png',
+      ));
     }
+
+    setState(() {});
   }
 
   @override
@@ -38,34 +36,36 @@ class _UsersListState extends State<UsersList> {
     getUsers();
   }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 102, 52, 116),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                // Ícone
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 50.0),
-                  child: Icon(
-                    Icons.supervisor_account_rounded,
-                    size: 100,
-                    color: Color.fromARGB(255, 252, 148, 25),
-                  ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 102, 52, 116),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              // Ícone
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 50.0),
+                child: Icon(
+                  Icons.supervisor_account_rounded,
+                  size: 100,
+                  color: Color.fromARGB(255, 252, 148, 25),
                 ),
-                // Sua ListView
-                Expanded(
-                  child: ListView(
-                    children:usList,
-                  ),
+              ),
+              // Sua ListView
+              Expanded(
+                child: ListView.builder(
+                  itemCount: usList.length,
+                  itemBuilder: (context, index) {
+                    return usList[index];
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
+  }
 }
-
